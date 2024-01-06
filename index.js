04dc2de1242d84ecece1aa2d002d2c1afb8ad874
Yash <yashghogre100@gmail.com>
2024-01-06 22:22:37 +0530

    some more minor changes

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const app = express();
 const hs = http.createServer(app)
 const io = require("socket.io")(hs, {
 	cors: {
-	//   origin: "http://localhost:3000",
-	  origin: "*",
-	  methods: ["GET", "POST"],
-	//   allowedHeaders: ["my-custom-header"],
-	  credentials: true
-	}
-  });
+		//   origin: "http://localhost:3000",
+		origin: true,
+		methods: ["GET", "POST"],
+		//   allowedHeaders: ["my-custom-header"],
+		credentials: true
+	},
+	allowEIO3: true,
+});
 
 app.use(express.json())
 // app.use(cors({origin: 'http://localhost:3000'}))
-app.use(cors({origin: 'https://chat-app-gold-delta.vercel.app/'}))
+app.use(cors({ origin: 'https://chat-app-gold-delta.vercel.app/' }))
 
-io.on("connection", (socket)=> {
+io.on("connection", (socket) => {
 	console.log("Socket.io connected", socket.id)
-	socket.on('msg', (msg)=> {
+	socket.on('msg', (msg) => {
 		console.log("New message", msg)
 		io.emit('msg', msg)
 	})
